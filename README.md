a70541a3eaccfd7d0c125a66d7d50105ea4d0e2c
Pepe Valdivia <jvaldivia@softweb.pe>
2018-06-04 17:56:24 -0500

    README agregado

---
+++ README.md
@@ -0,0 +1,7 @@
+# Proyecto Java Web con Netbeans
+
+---
+Fuentes: 
+
++ https://www.programmergate.com/pass-data-servlet-jsp/
++ https://studiofreya.com/2017/05/14/jsp-partials-how-to-include-files/
--- src/java/servlets/home/IndexHomeServlet.java
+++ src/java/servlets/home/IndexHomeServlet.java
@@ -13,6 +13,7 @@ public class IndexHomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
+        request.setAttribute("name", "Hussein Terek");
         request.getRequestDispatcher("home/index.jsp").forward(request, response);
     }
 }
