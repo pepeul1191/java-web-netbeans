f36d3b3bd35f0c11862749be1846053bc187c60b
Pepe Valdivia <jvaldivia@softweb.pe>
2018-06-04 19:56:54 -0500

    Agregando web/resources al path

--- src/java/servlets/home/IndexHomeServlet.java
+++ src/java/servlets/home/IndexHomeServlet.java
@@ -1,14 +1,11 @@
 package servlets.home;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "IndexHomeServlet", urlPatterns = {"/"})
 public class IndexHomeServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
--- web/home/index.jsp
+++ web/home/index.jsp
@@ -1,4 +1,6 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+  <link rel="stylesheet" href="${pageContext.request.contextPath}/resources/bower_components/swp-backbone/assets/css/dashboard.css" />
+
 <jsp:include page="../partials/blank_header.jsp">
   <jsp:param name="title" value="Main title" />
 </jsp:include>
